30e6eee75c82c741f4247ab698c89f0e0d2b0b8e
Vladislav.Yukhimenko <Vladislav.Yukhimenko@nexign.com>
2020-07-12 22:15:45 +0300

    Updates

---
+++ precache-manifest.7849ef841d6cd112ac32883cc11e2f17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbf9592d3ac45142143f0c2572805ae9",
+    "url": "/simple-chart-editor/index.html"
+  },
+  {
+    "revision": "cdd077553d0fbe61c57b",
+    "url": "/simple-chart-editor/static/css/main.ba683d45.chunk.css"
+  },
+  {
+    "revision": "e0b08499cc80fd9bcde4",
+    "url": "/simple-chart-editor/static/js/2.d5bca7c5.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/simple-chart-editor/static/js/2.d5bca7c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd077553d0fbe61c57b",
+    "url": "/simple-chart-editor/static/js/main.5c90f275.chunk.js"
+  },
+  {
+    "revision": "d4f5013d16f2db5dc568",
+    "url": "/simple-chart-editor/static/js/runtime-main.c441051f.js"
+  }
+]);
